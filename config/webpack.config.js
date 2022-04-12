f5648361bb6cd0205530f5dcac3ef401e2f19416
李昌南 <changnan666@gmail.com>
2022-04-12 10:50:19 +0800

    styled-components 显示类名

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -423,6 +423,7 @@ module.exports = function (webpackEnv) {
                   isEnvDevelopment &&
                   shouldUseReactRefresh &&
                   require.resolve('react-refresh/babel'),
+                  require.resolve('babel-plugin-styled-components'),
                 ].filter(Boolean),
                 // This is a feature of `babel-loader` for webpack (not Babel itself).
                 // It enables caching results in ./node_modules/.cache/babel-loader/
